4832839208a29e2f6a2e7d2061982c474cbf30dc
Immanuel <ipmaster40@gmail.com>
2025-03-28 23:01:05 +0300

    Finishing up

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        const body = document.querySelector("body");
+        const p = document.createElement("p");
+        p.textContent = `New User ID: ${object.id}`;
+        body.appendChild(p);
+      })
+      .catch(function (error) {
+        const body = document.querySelector("body");
+        const p = document.createElement("p");
+        p.textContent = `Error: ${error.message}`;
+        body.appendChild(p);
+      });
+  }
